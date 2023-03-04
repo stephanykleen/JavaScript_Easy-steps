b8bc37901584bcf9928e1bd646041fcb739b6608
Stephany <stephanykleen@gmail.com>
2023-03-03 22:42:48 -0800

    exercise 4
    testing self-invoking functions
    testing nested functions output

---
+++ 06-exercise/script.js
@@ -0,0 +1,11 @@
+
+let variable = (function(){
+  let local = 0;
+  const nested = function(){ return local = local + 1}
+  return nested
+})()
+
+
+console.log(variable())
+console.log(variable())
+console.log(variable())
