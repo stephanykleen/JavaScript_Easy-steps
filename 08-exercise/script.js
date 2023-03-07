5c7cfd556bc48db7687120c43024e857970bb1e9
Stephany <stephanykleen@gmail.com>
2023-03-06 21:45:24 -0800

    arithmetical operators exercises

---
+++ 08-exercise/script.js
@@ -0,0 +1,9 @@
+(function(){
+let sum = 2 + 2;console.log(`Addition: ${sum}`)
+let multiplication = sum * 7;console.log(`Multiplication: ${multiplication}`)
+let subtraction = multiplication - 4;console.log(`Subtraction: ${subtraction}`)
+let division = subtraction / 2; console.log(`Division: ${division}`)
+let mod = sum % 4;console.log(`Modulus: ${mod}`)
+let inc = ++sum; console.log(`Increment: ${inc}`)
+let dec = --sum; console.log(`Decrement: ${dec}`)
+})()
