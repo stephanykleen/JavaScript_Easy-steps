f4d981111f4aabf7542d768f66417441317fe9d7
Stephany <stephanykleen@gmail.com>
2023-03-06 21:55:58 -0800

    Assign values with operators
    practice exercises.

---
+++ 09-exercise/script.js
@@ -0,0 +1,11 @@
+(function(){
+  let str = 'This is a ';
+  str += 'string!'
+  console.log(str)
+  let sum = 1; sum+=2
+  console.log(sum)
+  let mul = 2; mul*=2
+  console.log(mul)
+  let mol = 3; mol%=2
+  console.log(mol)
+})()
