956c65991dbbad8755361026bad05760927a32b4
Stephany <stephanykleen@gmail.com>
2023-03-07 09:18:28 -0800

    Make comparisons among variables
    random exercises to practice making comparisons with logical operators

---
+++ 10-exercise/script.js
@@ -0,0 +1,11 @@
+(function(){
+  let comparison, number = 5, str = 'Javascript';
+  comparison = str === 'JAVASCRIPT'
+  console.log(comparison)
+  comparison = str === 'Javascript'
+  console.log(comparison)
+  comparison = number >= 7
+  console.log(comparison)
+  comparison = number < 9
+  console.log(comparison)
+})()
